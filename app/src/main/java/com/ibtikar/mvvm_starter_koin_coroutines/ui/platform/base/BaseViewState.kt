58a6cc195d29226c37fff21dc4d726df0d3f8e4f
KhaledAhmed-Ibtikar <56968158+KhaledAhmed-Ibtikar@users.noreply.github.com>
2021-02-21 14:34:43 +0200

    [Add] Explanation for Loaded Interface

--- app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/ui/platform/base/BaseViewState.kt
+++ app/src/main/java/com/ibtikar/mvvm_starter_koin_coroutines/ui/platform/base/BaseViewState.kt
@@ -5,6 +5,7 @@ import com.ibtikar.mvvm_starter_koin_coroutines.common.ApplicationRunTimeExcepti
 open class BaseViewState {
     data class Loading(val displayLoading: Boolean = true) : BaseViewState()
     data class Error(val applicationRunTimeException: ApplicationRunTimeException) : BaseViewState()
+    // Loaded Interface To make it an addition to any ViewState, and thus it can be handled solely in the overridden onLoaded() method in parent and child view
     interface Loaded {
         companion object {
             val instance = object : BaseViewState() { }
